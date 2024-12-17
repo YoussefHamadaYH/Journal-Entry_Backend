2e7b57b8cd5b1b591f15cc79be0caa4130ba6d1e
YoussefHamadaYH <youssefarqam772@gmail.com>
2024-12-17 22:53:32 +0200

    #modify dto

--- DTOs/JournalHeaderDTO.cs
+++ DTOs/JournalHeaderDTO.cs
@@ -1,11 +1,15 @@
 ﻿using JournyTask.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace JournyTask.DTOs
 {
     public class JournalHeaderDTO
     {
         public Guid Id { get; set; }
-        public  DateTimeOffset EntryDate  { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public  DateTime EntryDate  { get; set; }
+        [Required]
         public string Description { get; set; }
         public List<JournalDetailDTO> JournalDetails { get; set; } = new List<JournalDetailDTO>();
 
